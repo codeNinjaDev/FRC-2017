11658cf93b5ab6e159ff48363c0cc77821b7b74a
codeNinjaDev <workpraytoday@gmail.com>
2017-01-18 18:04:54 -0600

    added actions and driving for autonomous

---
+++ src/Auto/Action/Action.cpp
@@ -0,0 +1,14 @@
+/*
+ * Action.cpp
+ *
+ *  Created on: Jan 14, 2017
+ *      Author: peter
+ */
+
+#include <Auto/Action/Action.h>
+
+
+
+Action::~Action() {
+}
+
---
+++ src/Auto/AutoRoutine.cpp
@@ -0,0 +1,18 @@
+/*
+ * AutoRoutine.cpp
+ *
+ *  Created on: Jan 14, 2017
+ *      Author: peter
+ */
+
+#include <Auto/AutoRoutine.h>
+
+AutoRoutine::AutoRoutine() {
+  // TODO Auto-generated constructor stub
+
+}
+
+AutoRoutine::~AutoRoutine() {
+  // TODO Auto-generated destructor stub
+}
+
