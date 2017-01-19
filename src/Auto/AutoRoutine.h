9854ee0774c926fd1870b95b95d252cdeb23d831
codeNinjaDev <workpraytoday@gmail.com>
2017-01-18 18:04:54 -0600

    added actions and driving for autonomous

---
+++ src/Auto/AutoRoutine.h
@@ -0,0 +1,17 @@
+/*
+ * AutoRoutine.h
+ *
+ *  Created on: Jan 14, 2017
+ *      Author: peter
+ */
+
+#ifndef SRC_AUTO_AUTOROUTINE_H_
+#define SRC_AUTO_AUTOROUTINE_H_
+
+class AutoRoutine {
+public:
+  AutoRoutine();
+  virtual ~AutoRoutine();
+};
+
+#endif /* SRC_AUTO_AUTOROUTINE_H_ */
