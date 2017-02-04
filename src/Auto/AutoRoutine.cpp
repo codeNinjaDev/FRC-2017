a980c01055a4421725381a2a75dcea2931935155
codeNinjaDev <workpraytoday@gmail.com>
2017-02-04 14:04:23 -0600

    WPI Combined AutoBase and AutoRoutine.

--- src/Auto/AutoRoutine.cpp
+++ src/Auto/AutoRoutine.cpp
@@ -6,17 +6,46 @@
  */
 
 #include "AutoRoutine.h"
-#include "AutoBase.h"
 /*void AutoRoutine::WaitTime(double seconds) {
-  AutoBase::runAction(new TimeoutAction(seconds));
+  AutoRoutine::runAction(new TimeoutAction(seconds));
 }*/
+
+void AutoRoutine::Run() {
+  m_active = true;
+  /*try {
+    routine();
+  }*/
+  Routine();
+
+
+
+}
+
+void AutoRoutine::Stop() {
+  m_active = false;
+}
+
+bool AutoRoutine::IsActive() {
+  return m_active;
+}
+
+void AutoRoutine::RunAction(Action* action) {
+  action->Start();
+  while (IsActive() && !action->IsFinished()) {
+    action->Update();
+
+  }
+
+  action->Done();
+}
 
 void AutoRoutine::DriveInterval(double seconds, double y, double x) {
 
-  AutoBase::RunAction(new DriveIntervalAction(seconds, y, x));
+  RunAction(new DriveIntervalAction(seconds, y, x));
 }
 
 void AutoRoutine::Shoot(double seconds, double speed) {
-  AutoBase::RunAction(new ShootAction(seconds, speed));
+  RunAction(new ShootAction(seconds, speed));
 }
+
 
--- src/Auto/AutoRoutine.h
+++ src/Auto/AutoRoutine.h
@@ -8,16 +8,30 @@
 #ifndef SRC_AUTO_AUTOROUTINE_H_
 #define SRC_AUTO_AUTOROUTINE_H_
 
-#include "AutoBase.h"
+#include "WPILib.h"
+#include <iostream>
 #include "Action/Action.h"
 #include "Action/DriveIntervalAction.h"
 #include "Action/ShootAction.h"
 
 
-class AutoRoutine : public AutoBase {
+class AutoRoutine {
 public:
+  bool m_active = false;
+  virtual void Prestart();
+  void Run();
+  void Stop();
+  bool IsActive();
+  static constexpr double m_update_rate = (1.0/50.0);
+
+  //bool isActiveWithThrow() throw AutoRoutineEndedException;
+  void RunAction(Action* action);
   void DriveInterval(double seconds, double y, double x);
   void Shoot(double seconds, double speed);
+protected:
+  virtual void Routine();
+  //virtual void routine();
+
 };
 
 #endif /* SRC_AUTO_AUTOROUTINE_H_ */
